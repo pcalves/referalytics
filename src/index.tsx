fb34e5961a605aac7176982580f5cebc1d09117f
Paulo Coelho Alves <paulo@pcalv.es>
2024-04-28 13:55:20 +0100

    feat: Use Deno KV for storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,8 @@
 /** @jsx jsx */
 
-import { PathLike } from "node:fs";
-
 import { Hono } from "hono";
 import { jsx } from "hono/jsx";
-import { Low } from "lowdb";
-import { JSONFile } from "lowdb/node";
 import { Pages } from "./layout.tsx";
-
-async function JSONFilePreset<Data>(
-  filename: PathLike,
-  defaultData: Data,
-): Promise<Low<Data>> {
-  const adapter = new JSONFile<Data>(filename);
-  const db = new Low<Data>(adapter, defaultData);
-  await db.read();
-  return db;
-}
 
 export type Page = {
   slug: string;
@@ -27,13 +13,18 @@ type Data = {
   pages: Page[];
 };
 
-const db = await JSONFilePreset<Data>("db.json", { pages: [] });
-
-const { pages } = db.data;
+const kv = await Deno.openKv(".kv");
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
+  const entries = await kv.list<Page>({ prefix: ["pages"] });
+
+  const pages: Page[] = [];
+  for await (const { key, value } of entries) {
+    pages.push(value);
+  }
+
   const props = {
     site: Deno.env.get("SITE") || "https://www.example.com",
     pages,
@@ -54,19 +45,18 @@ app.get(
       return c.body("Missing parameter", 400);
     }
 
-    const page = pages.find((p) => p.slug === slug);
-
-    if (page && !page.referers.includes(referer)) {
-      await db.update(({ pages }) => {
-        pages[pages.indexOf(page)].referers = [...page.referers, referer];
+    const key = ["pages", slug];
+    const item = await kv.get<Page>(key);
+    if (item.value && !item.value.referers.includes(referer)) {
+      kv.set(key, {
+        slug,
+        referers: [...item.value.referers, referer],
       });
-    } else if (!page) {
-      await db.update(({ pages }) =>
-        pages.push({
-          slug,
-          referers: [referer],
-        })
-      );
+    } else {
+      kv.set(key, {
+        slug,
+        referers: [referer],
+      });
     }
 
     return c.body("Created", 201);
