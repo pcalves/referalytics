49cb8b519c2b9c1d4943eb38267750ea1ff77b8d
Paulo Coelho Alves <paulo@pcalv.es>
2024-04-30 21:57:29 +0100

    fix: Avoid referrals from the site

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,10 @@ type Data = {
 
 const kv = await Deno.openKv();
 
+const site = Deno.env.get("SITE_URL") || "https://www.example.com";
+const location = Deno.env.get("ANALYTICS_URL") ||
+  "https://analytics.example.com";
+
 const app = new Hono();
 
 app.get("/", async (c) => {
@@ -26,7 +30,7 @@ app.get("/", async (c) => {
   }
 
   const props = {
-    site: Deno.env.get("SITE_URL") || "https://www.example.com/",
+    site,
     pages,
   };
 
@@ -40,14 +44,12 @@ app.get(
   async (c) => {
     const slug = decodeURI(c.req.query("slug") ?? "");
     const referer = decodeURI(c.req.query("referer") ?? "");
-    const location = Deno.env.get("ANALYTICS_URL") ||
-      "https://analytics.example.com";
 
     if (!slug || !referer) {
       return c.body("Missing parameter", 400);
     }
 
-    if (referer.includes(location)) {
+    if (referer.includes(location) || referer.includes(site)) {
       return c.body("No self-referrals", 400);
     }
 
